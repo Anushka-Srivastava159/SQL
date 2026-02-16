1159156de9740c5ebf8092a3b380759dbeccb04d
Anushka <anushka.srivastava159@gmail.com>
2026-02-16 20:27:33 +0100

    Implement data checks and transformations for crm_prd_info
    Added checks and transformations for silver.crm_prd_info table, including handling missing data, validating dates, and standardizing product keys.

--- Scripts/silver/dataCleaning_transformation_silver.sql
+++ Scripts/silver/dataCleaning_transformation_silver.sql
@@ -1,5 +1,6 @@
-
-
+==============================================================================
+/*silver.crm_cust_info table checks and transformations*/  
+==============================================================================
 --check for nulls or duplicates in primary key
 --expectation: no result
 
@@ -57,3 +58,98 @@ from bronze.crm_cust_info
 --checking results
 
 select * from silver.crm_cust_info
+
+==============================================================================
+/*silver.crm_prd_info table checks and transformations*/  
+==============================================================================
+	
+--check for nulls or duplicates in primary key
+--expectation: no result
+select
+prd_id,
+COUNT(*)
+from silver.crm_prd_info
+group by prd_id
+having COUNT(*)>1 or prd_id is null
+
+-- extracting category id from product key in crm_prd_info table with erp_px_cat_g1v2 table id column
+select 
+prd_id,
+prd_key,
+replace(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id,
+SUBSTRING(prd_key, 7, len(prd_key)) as prd_key,
+prd_name,
+prd_cost,
+prd_line,
+prd_start_date,
+prd_end_date
+from bronze.crm_prd_info
+where replace(SUBSTRING(prd_key, 1, 5), '-', '_') not in 
+(select distinct id from bronze.erp_px_cat_g1v2)
+
+-- transforming prd_key in crm_prd_info table to match sls_prd_key in crm_sales_details table
+select 
+prd_id,
+prd_key,
+replace(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id,
+SUBSTRING(prd_key, 7, len(prd_key)) as prd_key,
+prd_name,
+prd_cost,
+prd_line,
+prd_start_date,
+prd_end_date
+from bronze.crm_prd_info
+where SUBSTRING(prd_key, 7, len(prd_key)) in 
+(select sls_prd_key from bronze.crm_sales_details)
+
+--handling mising data in prd_cost column
+select 
+prd_id,
+prd_key,
+replace(SUBSTRING(prd_key, 1, 5), '-', '_') as cat_id,
+SUBSTRING(prd_key, 7, len(prd_key)) as prd_key,
+prd_name,
+isnull(prd_cost, 0) as prd_cost,
+prd_line,
+prd_start_date,
+prd_end_date
+from bronze.crm_prd_info
+
+--check for unwanted spaces
+--expectation: no results
+
+select prd_name
+from silver.crm_prd_info
+where prd_name!=trim(prd_name)
+
+--checking quality of numbers
+--expectation: no results
+select prd_cost 
+from silver.crm_prd_info
+where prd_cost<0 or prd_cost is null
+
+--data standardization & consistency
+select distinct prd_line
+from silver.crm_prd_info
+
+--check for invalid date orders
+select *
+from silver	.crm_prd_info
+where prd_end_date<prd_start_date
+
+/* 200 rows where this scenario was true.
+in order to clean data so that is projects the right picture, pick few rows
+paste in excel and try to solve it. 
+solution: use start daye of next row, -1 it and put as end date.
+get it approved and integrate in query*/
+
+--Managing date columns
+select 
+prd_id,
+prd_key,
+prd_name,
+prd_start_date,
+prd_end_date,
+lead(prd_start_date) over (partition by prd_key order by prd_start_date)-1 as prd_end_date_test
+from bronze.crm_prd_info
+where prd_key in ('AC-HE-HL-U509-R', 'AC-HE-HL-U509')
